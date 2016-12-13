10f35f511d4b4b6ae3e76598da79ef8d2ec30fb7
yunanhelmi <yunanhelmimahendra@gmail.com>
2016-12-13 23:40:12 +0700

    Perbaikan nama file mpdf

--- config/web.php
+++ config/web.php
@@ -81,7 +81,7 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    //$config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
--- controllers/InstruksiKerjaController.php
+++ controllers/InstruksiKerjaController.php
@@ -294,7 +294,7 @@ class InstruksiKerjaController extends Controller
         // echo '</pre>';
         // exit();
 
-        $content = $this->render('_pdf', [
+        $content = $this->renderPartial('_pdf', [
             'model' => $model,
             'record' => $record,
         ]);
@@ -320,7 +320,8 @@ class InstruksiKerjaController extends Controller
                 'font-style'=> 'B',
                 'font-family' => 'serif',
                 'color'     => '#333333'
-            ]
+            ],
+            'line' => TRUE,
         ];
 
         $ourPdfFooter = [
@@ -349,14 +350,15 @@ class InstruksiKerjaController extends Controller
         ];
 
         $pdf = new \kartik\mpdf\Pdf([
-            //'mode' => \kartik\mpdf\Pdf::MODE_CORE,
+            'mode' => \kartik\mpdf\Pdf::MODE_CORE,
             'format' => \kartik\mpdf\Pdf::FORMAT_A4,
             'orientation' => \kartik\mpdf\Pdf::ORIENT_PORTRAIT,
             'destination' => \kartik\mpdf\Pdf::DEST_BROWSER,
             'content' => $content,
             'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
-            //'cssInline' => '.kv-heading-1{font-size:18px}',
-            'options' => ['title' => \Yii::$app->name],
+            'cssInline' => '.kv-heading-1{font-size:18px}',
+            'filename' => $model->case_number.'.'.'pdf',
+            'options' => ['title' => $model->case_number],
             'methods' => [
                 'SetHeader' => [
                     ['odd' => $ourPdfHeader, 'even' => $ourPdfHeader]
